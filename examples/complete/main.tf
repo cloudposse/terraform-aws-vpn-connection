76477916cd90fc801a6be9c023341c28c32ff334
John C. Bland II <johncblandii@gmail.com>
2020-04-28 05:01:45 -0500

    chore: upgrade to terraform 0.12

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,25 +1,25 @@
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
 module "vpn_connection" {
   source                                    = "../../"
-  enabled                                   = "${var.enabled}"
-  namespace                                 = "${var.namespace}"
-  stage                                     = "${var.stage}"
-  name                                      = "${var.name}"
-  delimiter                                 = "${var.delimiter}"
-  attributes                                = "${var.attributes}"
-  tags                                      = "${var.tags}"
-  vpc_id                                    = "${var.vpc_id}"
-  vpn_gateway_amazon_side_asn               = "${var.vpn_gateway_amazon_side_asn}"
-  customer_gateway_bgp_asn                  = "${var.customer_gateway_bgp_asn}"
-  customer_gateway_ip_address               = "${var.customer_gateway_ip_address}"
-  route_table_ids                           = ["${var.route_table_ids}"]
-  vpn_connection_static_routes_only         = "${var.vpn_connection_static_routes_only}"
-  vpn_connection_static_routes_destinations = "${var.vpn_connection_static_routes_destinations}"
-  vpn_connection_tunnel1_inside_cidr        = "${var.vpn_connection_tunnel1_inside_cidr}"
-  vpn_connection_tunnel2_inside_cidr        = "${var.vpn_connection_tunnel2_inside_cidr}"
-  vpn_connection_tunnel1_preshared_key      = "${var.vpn_connection_tunnel1_preshared_key}"
-  vpn_connection_tunnel2_preshared_key      = "${var.vpn_connection_tunnel2_preshared_key}"
+  enabled                                   = var.enabled
+  namespace                                 = var.namespace
+  stage                                     = var.stage
+  name                                      = var.name
+  delimiter                                 = var.delimiter
+  attributes                                = var.attributes
+  tags                                      = var.tags
+  vpc_id                                    = var.vpc_id
+  vpn_gateway_amazon_side_asn               = var.vpn_gateway_amazon_side_asn
+  customer_gateway_bgp_asn                  = var.customer_gateway_bgp_asn
+  customer_gateway_ip_address               = var.customer_gateway_ip_address
+  route_table_ids                           = [var.route_table_ids]
+  vpn_connection_static_routes_only         = var.vpn_connection_static_routes_only
+  vpn_connection_static_routes_destinations = var.vpn_connection_static_routes_destinations
+  vpn_connection_tunnel1_inside_cidr        = var.vpn_connection_tunnel1_inside_cidr
+  vpn_connection_tunnel2_inside_cidr        = var.vpn_connection_tunnel2_inside_cidr
+  vpn_connection_tunnel1_preshared_key      = var.vpn_connection_tunnel1_preshared_key
+  vpn_connection_tunnel2_preshared_key      = var.vpn_connection_tunnel2_preshared_key
 }
--- variables.tf
+++ variables.tf
@@ -1,44 +1,44 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `eg` or `cp`)"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Name  (e.g. `app`)"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. `{ BusinessUnit = \"XYZ\" }`"
 }
 
 variable "enabled" {
-  type        = "string"
+  type        = string
   description = "Set to `false` to prevent the module from creating any resources"
   default     = "true"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "The ID of the VPC to which the Virtual Private Gateway will be attached"
 }
 
@@ -53,48 +53,49 @@ variable "customer_gateway_bgp_asn" {
 }
 
 variable "customer_gateway_ip_address" {
-  type        = "string"
+  type        = string
   description = "The IP address of the gateway's Internet-routable external interface"
 }
 
 variable "route_table_ids" {
-  type        = "list"
+  type        = list(string)
   description = "The IDs of the route tables for which routes from the Virtual Private Gateway will be propagated"
   default     = []
 }
 
 variable "vpn_connection_static_routes_only" {
-  type        = "string"
+  type        = string
   description = "If set to `true`, the VPN connection will use static routes exclusively. Static routes must be used for devices that don't support BGP"
   default     = "true"
 }
 
 variable "vpn_connection_static_routes_destinations" {
-  type        = "list"
+  type        = list(string)
   description = "List of CIDR blocks to be used as destination for static routes. Routes to destinations will be propagated to the route tables defined in `route_table_ids`"
   default     = []
 }
 
 variable "vpn_connection_tunnel1_inside_cidr" {
-  type        = "string"
+  type        = string
   description = "The CIDR block of the inside IP addresses for the first VPN tunnel"
   default     = ""
 }
 
 variable "vpn_connection_tunnel2_inside_cidr" {
-  type        = "string"
+  type        = string
   description = "The CIDR block of the inside IP addresses for the second VPN tunnel"
   default     = ""
 }
 
 variable "vpn_connection_tunnel1_preshared_key" {
-  type        = "string"
+  type        = string
   description = "The preshared key of the first VPN tunnel. The preshared key must be between 8 and 64 characters in length and cannot start with zero. Allowed characters are alphanumeric characters, periods(.) and underscores(_)"
   default     = ""
 }
 
 variable "vpn_connection_tunnel2_preshared_key" {
-  type        = "string"
+  type        = string
   description = "The preshared key of the second VPN tunnel. The preshared key must be between 8 and 64 characters in length and cannot start with zero. Allowed characters are alphanumeric characters, periods(.) and underscores(_)"
   default     = ""
 }
+
