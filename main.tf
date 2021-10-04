fa96c11b163ffb02541d0e1d79af0add22f379da
Veronika Gnilitska <veronika.gnilitska@gmail.com>
2021-10-04 16:56:15 +0300

    feat: adds new optional arguments

--- main.tf
+++ main.tf
@@ -21,16 +21,41 @@ resource "aws_customer_gateway" "default" {
 
 # https://www.terraform.io/docs/providers/aws/r/vpn_connection.html
 resource "aws_vpn_connection" "default" {
-  count                 = local.enabled ? 1 : 0
-  vpn_gateway_id        = join("", aws_vpn_gateway.default.*.id)
-  customer_gateway_id   = join("", aws_customer_gateway.default.*.id)
-  type                  = "ipsec.1"
-  static_routes_only    = var.vpn_connection_static_routes_only
-  tunnel1_inside_cidr   = var.vpn_connection_tunnel1_inside_cidr
-  tunnel2_inside_cidr   = var.vpn_connection_tunnel2_inside_cidr
-  tunnel1_preshared_key = var.vpn_connection_tunnel1_preshared_key
-  tunnel2_preshared_key = var.vpn_connection_tunnel2_preshared_key
-  tags                  = module.this.tags
+  count                    = local.enabled ? 1 : 0
+  vpn_gateway_id           = join("", aws_vpn_gateway.default.*.id)
+  customer_gateway_id      = join("", aws_customer_gateway.default.*.id)
+  type                     = "ipsec.1"
+  static_routes_only       = var.vpn_connection_static_routes_only
+  local_ipv4_network_cidr  = var.vpn_connection_local_ipv4_network_cidr
+  remote_ipv4_network_cidr = var.vpn_connection_remote_ipv4_network_cidr
+
+  tunnel1_dpd_timeout_action = var.vpn_connection_tunnel1_dpd_timeout_action
+  tunnel1_ike_versions       = var.vpn_connection_tunnel1_ike_versions
+  tunnel1_inside_cidr        = var.vpn_connection_tunnel1_inside_cidr
+  tunnel1_preshared_key      = var.vpn_connection_tunnel1_preshared_key
+  tunnel1_startup_action     = var.vpn_connection_tunnel1_startup_action
+
+  tunnel1_phase1_dh_group_numbers      = var.vpn_connection_tunnel1_phase1_dh_group_numbers
+  tunnel1_phase2_dh_group_numbers      = var.vpn_connection_tunnel1_phase2_dh_group_numbers
+  tunnel1_phase1_encryption_algorithms = var.vpn_connection_tunnel1_phase1_encryption_algorithms
+  tunnel1_phase2_encryption_algorithms = var.vpn_connection_tunnel1_phase2_encryption_algorithms
+  tunnel1_phase1_integrity_algorithms  = var.vpn_connection_tunnel1_phase1_integrity_algorithms
+  tunnel1_phase2_integrity_algorithms  = var.vpn_connection_tunnel1_phase2_integrity_algorithms
+
+  tunnel2_dpd_timeout_action = var.vpn_connection_tunnel2_dpd_timeout_action
+  tunnel2_ike_versions       = var.vpn_connection_tunnel2_ike_versions
+  tunnel2_inside_cidr        = var.vpn_connection_tunnel2_inside_cidr
+  tunnel2_preshared_key      = var.vpn_connection_tunnel2_preshared_key
+  tunnel2_startup_action     = var.vpn_connection_tunnel2_startup_action
+
+  tunnel2_phase1_dh_group_numbers      = var.vpn_connection_tunnel2_phase1_dh_group_numbers
+  tunnel2_phase2_dh_group_numbers      = var.vpn_connection_tunnel2_phase2_dh_group_numbers
+  tunnel2_phase1_encryption_algorithms = var.vpn_connection_tunnel2_phase1_encryption_algorithms
+  tunnel2_phase2_encryption_algorithms = var.vpn_connection_tunnel2_phase2_encryption_algorithms
+  tunnel2_phase1_integrity_algorithms  = var.vpn_connection_tunnel2_phase1_integrity_algorithms
+  tunnel2_phase2_integrity_algorithms  = var.vpn_connection_tunnel2_phase2_integrity_algorithms
+
+  tags = module.this.tags
 }
 
 # https://www.terraform.io/docs/providers/aws/r/vpn_gateway_route_propagation.html
