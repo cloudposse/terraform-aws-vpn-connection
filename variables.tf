555c21bfc7dfe4ee65062cb75775bebead41252d
Matt Gowie <matt@masterpoint.io>
2024-02-19 18:53:00 -0700

    chore: cleans up tflint issues 🎉

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "vpc_id" {
 }
 
 variable "vpn_gateway_amazon_side_asn" {
+  type        = number
   description = "The Autonomous System Number (ASN) for the Amazon side of the VPN gateway. If you don't specify an ASN, the Virtual Private Gateway is created with the default ASN"
   default     = 64512
 }
